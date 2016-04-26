578b0b27cf93071a7784993050828afa8ce00918
Latrell Chan <i@latrell.me>
2016-04-26 13:18:12 +0800

    取消延迟加载。

--- src/Latrell/AliyunMns/MnsServiceProvider.php
+++ src/Latrell/AliyunMns/MnsServiceProvider.php
@@ -12,41 +12,14 @@ class MnsServiceProvider extends ServiceProvider
 {
 
 	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
-
-	/**
 	 * Register the application services.
 	 *
 	 * @return void
 	 */
 	public function register()
 	{
-		$this->app['queue']->addConnector('mns', function () {
+		$this->app['queue']->extend('mns', function () {
 			return new MnsConnector();
 		});
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [
-			'queue',
-			'queue.worker',
-			'queue.listener',
-			'queue.failer',
-			'command.queue.work',
-			'command.queue.listen',
-			'command.queue.restart',
-			'command.queue.subscribe',
-			'queue.connection'
-		];
 	}
 }
